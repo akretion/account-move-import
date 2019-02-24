e843de9554de672d7a0c1912c642d3a3b3a2d0c5
Alexis de Lattre <alexis.delattre@akretion.com>
2019-02-24 14:04:48 +0100

    Update module description

--- account_move_csv_import/__manifest__.py
+++ account_move_csv_import/__manifest__.py
@@ -13,13 +13,19 @@
 Account Move Import
 ===================
 
-This module handle the import of one or several account moves from a file (CSV, TXT or any other format). The name of this module is account_move_csv_import, but it is not limited to the import of CSV files (this is an historic name and I didn't want to change it). This module currently supports:
+This module handle the import of one or several account moves from a file (CSV, TXT, XLS or any other format). The name of this module is account_move_csv_import, but it is not limited to the import of CSV files (this is an historic name and I didn't want to change it). This module currently supports:
 
+* LibreOffice CSV export files,
 * MeilleureGestion.com payroll CSV files,
 * Quadra export files,
-* LibreOffice CSV export files.
+* In Extenso,
+* Ciel paye,
+* Payfit,
+* FEC (text format).
 
 This module can easily be extended to support other formats.
+
+This module also supports account move line reconciliation (used for FEC import).
 
 There are many community modules that handle the import of account moves via CSV/TXT files. But I decided to develop this module because I wanted a module with the following design guidelines:
 
