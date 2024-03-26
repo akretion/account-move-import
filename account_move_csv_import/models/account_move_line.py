edb77281f79e2795c673178a608f19ba9e5fde33
hparfr <raphael.reverdy@akretion.com>
2024-03-26 17:11:36 +0100

    account_move_csv_import: linting in v16

--- account_move_csv_import/models/account_move_line.py
+++ account_move_csv_import/models/account_move_line.py
@@ -8,9 +8,10 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    import_reconcile = fields.Char(string='Import Reconcile Ref')
+    import_reconcile = fields.Char(string="Import Reconcile Ref")
     import_external_id = fields.Char(
         string="Import External ID",
-        help='Can be used to tag imported journal items. '
-             'Can be useful to delete imported journal items in case of '
-             'error on the imported file.')
+        help="Can be used to tag imported journal items. "
+        "Can be useful to delete imported journal items in case of "
+        "error on the imported file.",
+    )
