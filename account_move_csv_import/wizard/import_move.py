400420c8e5d801974962a033f87237f7c1487d6f
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2026-03-10 09:50:18 +0100

    Issue69429: Handle analytic tags

--- account_move_csv_import/wizard/import_move.py
+++ account_move_csv_import/wizard/import_move.py
@@ -716,6 +716,8 @@ class AccountMoveImport(models.TransientModel):
             'analytic_account_id': pivot_line.get('analytic_account_id'),
             'import_reconcile': pivot_line.get('reconcile_ref'),
             }
+        if pivot_line.get('analytic_tag_ids'):
+            vals['analytic_tag_ids'] = pivot_line.get('analytic_tag_ids')
         return vals
 
     def reconcile_move_lines(self, moves):
